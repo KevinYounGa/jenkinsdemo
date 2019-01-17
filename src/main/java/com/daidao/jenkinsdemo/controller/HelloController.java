78c0a2de337a894e5d40c88b9d72a289a112e2bf
KevinYounGa <ouyangjia121@gmail.com>
2019-01-17 16:09:55 +0800

    init

---
+++ src/main/java/com/daidao/jenkinsdemo/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.daidao.jenkinsdemo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+public class HelloController {
+    @RequestMapping("/")
+    public String sayHello(){
+        //修改返回的字符串的内容
+        return "123456. Hello jenkins, " + new Date();
+    }
+}
